0d0d38b6617862a693ad60b730913505eac748f3
Web Weaver <acharyapraidha08>
2025-04-11 18:28:56 +0545

    added timestamps

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -8,30 +8,33 @@ export enum ROLE {
   ORDERADMIN = "ORDER_ADMIN",
 }
 
-const users = new Schema({
-  firstName: { type: String, require: true },
-  lastName: { type: String, require: true },
-  email: { type: String, require: true, unique: true },
-  isVerified: { type: Boolean, default: false },
-  phoneNumber: { type: String, default: null },
-  password: { type: String, require: true },
-  additionalPhoneNumber: { type: String, default: null },
-  role: {
-    type: String,
-    enum: [ROLE.CUSTOMER, ROLE.ORDERADMIN, ROLE.SUPERADMIN],
-  },
-  shippingAddress: [
-    {
-      street: { type: String, default: null },
-      city: { type: String, default: null },
-      state: { type: String, default: null },
-      zip: { type: String, default: null },
-      country: { type: String, default: null },
-      landmark: { type: String, default: null },
-      area: { type: String, default: null },
-      id: { type: String, default: null },
+const users = new Schema(
+  {
+    firstName: { type: String, require: true },
+    lastName: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
+    isVerified: { type: Boolean, default: false },
+    phoneNumber: { type: String, default: null },
+    password: { type: String, require: true },
+    additionalPhoneNumber: { type: String, default: null },
+    role: {
+      type: String,
+      enum: [ROLE.CUSTOMER, ROLE.ORDERADMIN, ROLE.SUPERADMIN],
     },
-  ],
-});
+    shippingAddress: [
+      {
+        street: { type: String, default: null },
+        city: { type: String, default: null },
+        state: { type: String, default: null },
+        zip: { type: String, default: null },
+        country: { type: String, default: null },
+        landmark: { type: String, default: null },
+        area: { type: String, default: null },
+        id: { type: String, default: null },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 export const UserModel = model("Users", users);
